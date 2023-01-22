dc1efa34560febc76b8ed0b0e1ef17e39285cac5
Jeeva111 <jeeva.deee111@gmail.com>
2023-01-22 23:28:26 +0530

    Growing snake body

--- Assets/Script/Snake.cs
+++ Assets/Script/Snake.cs
@@ -7,6 +7,8 @@ public class Snake : MonoBehaviour
     [SerializeField]
     [Range(0,10)]
     private float speed = 0.5f;
+    [SerializeField]
+    private GameObject bodyPrefab;
     private Vector2Int gridPosition;
     private Vector2Int gridMoveDirection;
     private float gridMoveTimer;
@@ -14,12 +16,15 @@ public class Snake : MonoBehaviour
 
     private GameManager gameManager;
     private LevelHandler levelHandler;
+    private int snakeBodySize = 1;
+    private List<Vector2Int> snakeBodyPosition;
 
     private void Awake() {
         gridPosition = new Vector2Int(1, 1);
         gridMoveTimerMax = 0.5f;
         gridMoveTimer = gridMoveTimerMax;
         gridMoveDirection = new Vector2Int(1, 0);
+        snakeBodyPosition = new List<Vector2Int>();
     }
 
     private void Start() {
@@ -38,12 +43,22 @@ public class Snake : MonoBehaviour
         gridMoveTimer += Time.deltaTime;
         if (gridMoveTimer >= gridMoveTimerMax)
         {
-            gridPosition += gridMoveDirection;
             gridMoveTimer -= gridMoveTimerMax;
+            snakeBodyPosition.Insert(0, gridPosition);
+            gridPosition += gridMoveDirection;
+            if(snakeBodyPosition.Count >= snakeBodySize + 1) {
+                snakeBodyPosition.RemoveAt(snakeBodyPosition.Count - 1);
+            }
+
+            for(int i = 0; i < snakeBodyPosition.Count; i++) {
+                Vector2Int snakeMovePosition = snakeBodyPosition[i];
+                GameObject.Instantiate(bodyPrefab, new Vector2(snakeMovePosition.x, snakeMovePosition.y) * gridMoveTimerMax, Quaternion.identity);
+            }
+
+            transform.position = new Vector3(gridPosition.x * gameManager.gridMove, gridPosition.y * gameManager.gridMove);
+            transform.eulerAngles = new Vector3(0, 0, GetAngleFromVector(gridMoveDirection));
+            levelHandler.DetectObject(transform.position);
         }
-        transform.position = new Vector3(gridPosition.x * gameManager.gridMove, gridPosition.y * gameManager.gridMove);
-        transform.eulerAngles = new Vector3(0, 0, GetAngleFromVector(gridMoveDirection));
-        levelHandler.DetectObject(transform.position);
     }
 
     private void KeyboardInput()
